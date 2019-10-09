1d35c193cea344caf49cf72473de3824a055db5f
AderbalFilho <aderbalcrato@yahoo.com.br>
2019-10-09 13:29:36 -0300

    feat(exercise4): Solving exercise

---
+++ Exercise 4/app.js
@@ -0,0 +1,32 @@
+new Vue({
+  el: '#exercise',
+  data: {
+    highlight: false,
+    shrink: false,
+    classes: '',
+    classes2: '',
+    boolean: false,
+    styles: '',
+    progress: 0
+  },
+  watch: {
+    boolean: function() {
+      this.boolean === "false" ? this.boolean = false : this.boolean = this.boolean;
+    }
+  },
+  methods: {
+    startEffect: function() {
+      var vm = this;
+      setInterval(function() {
+        vm.highlight = !vm.highlight;
+        vm.highlight ? vm.shrink = false : vm.shrink = true;
+      }, 2000);
+    },
+    startProgress: function() {
+      var vm = this;
+      setInterval(function() {
+        vm.progress < 100 ? vm.progress += 1 : vm.progress = 100;
+      }, 500);
+    }
+  }
+});
