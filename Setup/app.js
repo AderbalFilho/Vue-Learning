e4b8880cee0166b2946aabc8e1c59f891b645979
AderbalFilho <aderbalcrato@yahoo.com.br>
2019-10-11 14:21:00 -0300

    feat(setup): Solving exercise

---
+++ Setup/app.js
@@ -0,0 +1,71 @@
+new Vue({
+    el: '#app',
+    data: {
+        healthYou: 100,
+        healthMonster: 100,
+        battleStarted: false,
+        battleHistory: [],
+        hasHistory: false
+    },
+    methods: {
+        attack: function() {
+            var damage = Math.floor(Math.random() * 5) + 6;
+            this.healthMonster -= damage;
+            this.battleHistory.push({
+                information: 'PLAYER HITS PLAYER FOR ' + damage,
+                turn: 'player'
+            })
+            if ( !this.isWon() ) {
+                this.monsterAttack();
+            }
+        },
+        battleStart: function() {
+            this.healthYou = 100;
+            this.healthMonster = 100;
+            this.battleHistory = [];
+            this.hasHistory = false;
+            this.battleStarted = true;
+        },
+        heal: function() {
+            var heal = Math.floor(Math.random() * 5) + 6;
+            this.healthYou += heal;
+            this.battleHistory.push({
+                information: 'PLAYER HEALS HIMSELF FOR ' + heal,
+                turn: 'player'
+            })
+            this.monsterAttack();
+        },
+        isWon: function() {
+            if ( this.healthMonster <= 0 ) {
+                this.healthMonster = 0;
+                confirm("You win! New game?") ? this.battleStart() : this.battleStarted = false;
+                return true;
+            }
+            return false;
+        },
+        monsterAttack: function() {
+            var monsterAttack = Math.floor(Math.random() * 7) + 6;
+            this.healthYou -= monsterAttack;
+            this.hasHistory = true;
+            this.battleHistory.push({
+                information: 'MONSTER HITS PLAYER FOR ' + monsterAttack,
+                turn: 'monster'
+            })
+            if ( this.healthYou <= 0 ) {
+                this.healthYou = 0;
+                confirm("You died! New game?") ? this.battleStart() : this.battleStarted = false;
+            }
+        },
+        specialAttack: function() {
+            var damage = Math.floor(Math.random() * 11) + 10;
+            this.healthMonster -= damage;
+            this.battleHistory.push({
+                information: 'PLAYER HITS PLAYER FOR ' + damage,
+                turn: 'player'
+            })
+            if ( !this.isWon() ) {
+                this.monsterAttack();
+            }
+        }
+    }
+})
